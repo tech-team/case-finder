77fdc55c7a2771325665069fb092f6af0af1a7be
alekseyl1992 <alekseyl@list.ru>
2014-07-13 18:46:29 +0400

    Maven + JavaFX project created

---
+++ src/main/java/gui/Main.java
@@ -0,0 +1,25 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/fxml/main.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Case Finder");
+        primaryStage.setScene(new Scene(root, 600, 400));
+
+        MainController controller = loader.getController();
+        //controller.initWithData(primaryStage, stack);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
