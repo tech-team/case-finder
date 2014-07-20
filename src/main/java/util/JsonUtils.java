37fd1a495ce6ad30e7fd6e682213424041544557
igorcoding <igorcoding@gmail.com>
2014-07-21 01:01:21 +0400

    Fixed JsonUtils bug

--- src/main/java/util/JsonUtils.java
+++ src/main/java/util/JsonUtils.java
@@ -1,35 +1,34 @@
 package util;
 
-import com.sun.istack.internal.NotNull;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public abstract class JsonUtils {
-    public static String getString(@NotNull JSONObject obj, @NotNull String key) {
+    public static String getString(JSONObject obj, String key) {
         return obj.isNull(key) ? null : obj.getString(key);
     }
 
-    public static Boolean getBoolean(@NotNull JSONObject obj, @NotNull String key) {
+    public static Boolean getBoolean(JSONObject obj, String key) {
         return obj.isNull(key) ? null : obj.getBoolean(key);
     }
 
-    public static Integer getInteger(@NotNull JSONObject obj, @NotNull String key) {
+    public static Integer getInteger(JSONObject obj, String key) {
         return obj.isNull(key) ? null : obj.getInt(key);
     }
 
-    public static JSONObject getJSONObject(@NotNull JSONObject obj, @NotNull String key) {
+    public static JSONObject getJSONObject(JSONObject obj, String key) {
         return obj.isNull(key) ? null : obj.getJSONObject(key);
     }
 
-    public static JSONArray getJSONArray(@NotNull JSONObject obj, @NotNull String key) {
+    public static JSONArray getJSONArray(JSONObject obj, String key) {
         return obj.isNull(key) ? null : obj.getJSONArray(key);
     }
 
-    public static Object getObject(@NotNull JSONObject obj, @NotNull String key) {
+    public static Object getObject(JSONObject obj, String key) {
         return obj.isNull(key) ? null : obj.get(key);
     }
 
-    public static Double getDouble(@NotNull JSONObject obj, @NotNull String key) {
+    public static Double getDouble(JSONObject obj, String key) {
         return obj.isNull(key) ? null : obj.getDouble(key);
     }
 }
