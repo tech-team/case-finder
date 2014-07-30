3c8f2f9f1f8bbd27485282ec2b29580cfd77cd49
alekseyl1992 <alekseyl@list.ru>
2014-07-30 21:45:41 +0400

    Saving width, height and maximized properties (closes #3)

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -5,9 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+import java.util.prefs.Preferences;
 
 public class Main extends Application {
     public static Application instance;
+    private static final String MAXIMIZED_PROPERTY = "maximized";
+    private static final String WIDTH_PROPERTY = "width";
+    private static final String HEIGHT_PROPERTY = "height";
 
     public Main() {
         instance = this;
@@ -15,9 +21,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Preferences prefs = Preferences.userNodeForPackage(Main.class);
+
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("/fxml/main.fxml"));
         Parent root = loader.load();
-        Scene scene = new Scene(root, 1024, 768);
+
+        double width = prefs.getDouble(WIDTH_PROPERTY, 1024);
+        double height = prefs.getDouble(HEIGHT_PROPERTY, 768);
+
+        Scene scene = new Scene(root, width, height);
         scene.getStylesheets().add("css/styles.css");
 
         primaryStage.setTitle("Case Finder");
@@ -25,8 +37,17 @@ public class Main extends Application {
 
         MainController controller = loader.getController();
         controller.setStage(primaryStage);
-        primaryStage.setOnCloseRequest(controller::onClose);
+        primaryStage.setOnCloseRequest((WindowEvent event) -> {
+            controller.onClose(event);
 
+            if (!event.isConsumed()) {
+                prefs.putBoolean(MAXIMIZED_PROPERTY, primaryStage.isMaximized());
+                prefs.putDouble(WIDTH_PROPERTY, primaryStage.getWidth());
+                prefs.putDouble(HEIGHT_PROPERTY, primaryStage.getHeight());
+            }
+        });
+
+        primaryStage.setMaximized(prefs.getBoolean(MAXIMIZED_PROPERTY, true));
         primaryStage.show();
     }
 
