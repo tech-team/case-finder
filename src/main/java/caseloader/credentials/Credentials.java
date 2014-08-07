5a740bad16e8516eae22a98230c97571d27f75f0
igorcoding <igorcoding@gmail.com>
2014-08-07 17:51:54 +0400

    Changed Credentials storage

--- src/main/java/caseloader/credentials/Credentials.java
+++ src/main/java/caseloader/credentials/Credentials.java
@@ -1,43 +1,25 @@
 package caseloader.credentials;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Credentials {
-    private class Pair {
-        private String website;
-        private String value;
-
-        private Pair(String website, String value) {
-            this.website = website;
-            this.value = value;
-        }
-
-        public String getWebsite() {
-            return website;
-        }
-
-        public String getValue() {
-            return value;
-        }
-    }
-
-    private List<Pair> directors = new LinkedList<>();
-    private List<Pair> telephones = new LinkedList<>();
+    private Map<String, List<String>> directors = new HashMap<>();
+    private Map<String, List<String>> telephones = new HashMap<>();
     private String inn = null;
     private String ogrn = null;
 
     public void addDirector(String website, String director) {
         if (director != null) {
-            directors.add(new Pair(website, director));
+            List<String> directors = this.directors.get(website);
+            directors.add(director);
         }
     }
 
     public void addTelephone(String website, String telephone) {
         if (telephone != null) {
-            telephones.add(new Pair(website, telephone));
+            List<String> telephones = this.telephones.get(website);
+            telephones.add(telephone);
         }
     }
 
@@ -85,36 +67,34 @@ public class Credentials {
         }
     }
 
-    public List<String> getDirectors(String website) {
-        List<String> websiteDirectors
-                            = directors.stream()
-                                       .filter(d -> d.getWebsite().equals(website))
-                                       .map(Pair::getValue)
-                                       .collect(Collectors.toCollection(LinkedList::new));
-//        String[] res = new String[websiteDirectors.size()];
-//        return websiteDirectors.toArray(res);
-        return websiteDirectors;
+    public Map<String, List<String>> getDirectors() {
+        return directors;
     }
 
-    public List<Pair> getTelephones() {
+    public List<String> getDirectors(String website) {
+        return directors.get(website);
+    }
+
+    public Map<String, List<String>> getTelephones() {
         return telephones;
     }
 
     public List<String> getTelephones(String website) {
-        List<String> websiteTelephones
-                = telephones.stream()
-                .filter(d -> d.getWebsite().equals(website))
-                .map(Pair::getValue)
-                .collect(Collectors.toCollection(LinkedList::new));
-//        String[] res = new String[websiteTelephones.size()];
-//        return websiteTelephones.toArray(res);
-        return websiteTelephones;
+        return telephones.get(website);
     }
 
     public void merge(final Credentials found) {
         if (found != null) {
-            directors.addAll(found.directors);
-            telephones.addAll(found.telephones);
+            for (Map.Entry<String, List<String>> directors : found.directors.entrySet()) {
+                for (String d : directors.getValue()) {
+                    addDirector(directors.getKey(), d);
+                }
+            }
+            for (Map.Entry<String, List<String>> telephones : found.telephones.entrySet()) {
+                for (String t : telephones.getValue()) {
+                    addTelephone(telephones.getKey(), t);
+                }
+            }
 
             if (!hasInn()) {
                 setInn(found.getInn());
