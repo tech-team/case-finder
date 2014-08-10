6f677deedc3869cd5223bdc1361a44d44fe518aa
alekseyl1992 <alekseyl@list.ru>
2014-08-10 13:58:32 +0400

    Localized ErrorReason

--- src/main/java/caseloader/errors/ErrorReason.java
+++ src/main/java/caseloader/errors/ErrorReason.java
@@ -1,7 +1,24 @@
 package caseloader.errors;
 
+import util.ResourceControl;
+
+import java.util.ResourceBundle;
+
 public enum ErrorReason {
-    KAD_PAGE_ERROR,
-    COURTS_RETRIEVAL_ERROR,
-    UNEXPECTED_ERROR
+    KAD_PAGE_ERROR("kadPageError"),
+    COURTS_RETRIEVAL_ERROR("courtsRetrievalError"),
+    UNEXPECTED_ERROR("unexpectedError");
+
+    private String descResString;
+
+    ErrorReason(String descResString) {
+        this.descResString = descResString;
+    }
+
+    public String getLocalizedDescription() {
+        ResourceBundle res = ResourceBundle.getBundle("properties.error_strings",
+                new ResourceControl("UTF-8"));
+
+        return res.getString(descResString);
+    }
 }
